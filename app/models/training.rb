9fb6960f5b003fcd7ba7e7f1bc811a323bf8351d
gregoire placca <gregoire.placca@hotmail.com>
2020-01-30 20:42:00 +0100

    generated trainings, training exercises and game exercise players controllers

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,6 +1,7 @@
 class Training < ApplicationRecord
   belongs_to :team
   has_many :exercises, through: :training_exercises
+  has_many :training_exercises, dependent: :destroy
   validates :starts_at, presence: true
   validates :duration_in_minutes, presence: true
 end
---
+++ test/controllers/game_exercise_players_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GameExercisePlayersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
