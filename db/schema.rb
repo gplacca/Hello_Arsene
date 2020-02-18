a964a5fabb35e0e84ceeb0e56f2bcb86e9da2963
gregoire placca <gregoire.placca@hotmail.com>
2020-02-18 19:18:27 +0100

    chaging the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_202412) do
+ActiveRecord::Schema.define(version: 2020_02_17_181154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_202412) do
     t.bigint "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number"
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,22 +19,23 @@ Exercise.destroy_all
 
 user = User.create!(email: "contact@helloarsene.com", password: "helloarsene")
 
-fc_gobelins = Team.create!(name: "Nationale 2", club_name: "FC Gobelins Paris XIII", user: user)
+le_wagon = Team.create!(name: "Code League", club_name: "Le Wagon FC", user: user)
 # fc_gobelins.photo = "https://res.cloudinary.com/dbuix43pi/image/upload/v1581448597/Logo_FC_Gobelins_wigdn0.png"
 # fc_gobelins.save!
 
-csv_t = File.read(Rails.root.join('lib', 'exercises.csv'))
-csv = CSV.parse(csv_t, :headers => true)
+csv_t = File.read(Rails.root.join('lib', 'exercises_3.csv'))
+csv = CSV.parse(csv_t, :headers => true, col_sep: ';')
 csv.each do |row|
-  Exercise.create!(name: row[0], description: row[1], default_duration_in_minutes: row[2])
+  Exercise.create!(name: row[0], description: row[1], default_duration_in_minutes: row[2], category: row[3])
 end
 puts "Tu as créé des exercices"
 
 csv_players = File.read(Rails.root.join('lib', 'players.csv'))
-p csv_p = CSV.parse(csv_players, :headers => true)
+csv_p = CSV.parse(csv_players, :headers => true, col_sep: ';')
 csv_p.each do |row|
-  p row
-  Player.create!(first_name: row[0], last_name: row[1], birth_date: row[2], post: row[3], team: fc_gobelins)
+  Player.create!(first_name: row[0], last_name: row[1], post: row[2], number: row[3], team: le_wagon)
 end
 puts "Tu as créé des joueurs"
 
+
+# encoding:'iso-8859-1:utf-8',
