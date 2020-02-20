6359cbbbf0cdfa4684ab7dc14460596597e77d19
gregoire placca <gregoire.placca@hotmail.com>
2020-02-20 22:06:09 +0100

    exercises

--- app/models/training.rb
+++ app/models/training.rb
@@ -4,4 +4,12 @@ class Training < ApplicationRecord
   has_many :training_exercises, dependent: :destroy
   validates :start_at, presence: true
   validates :duration_in_minutes, presence: true
+
+  def total_time
+    self.training_exercises.pluck(:duration_in_minutes).reduce(:+)
+  end
+
+  def total_perc
+    (total_time.to_f / self.duration_in_minutes * 100) || 0
+  end
 end
