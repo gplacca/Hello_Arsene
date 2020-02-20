a173248332585fa814607d9589c0eeb39af1d5da
gregoire placca <gregoire.placca@hotmail.com>
2020-02-20 19:03:47 +0100

    exercises list

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,8 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @training = Training.new
     @last_five_trainings = @team.trainings.last(5)
+
+    @players = Player.all
   end
 
   def edit
