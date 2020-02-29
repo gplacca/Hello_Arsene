e4c66b57d1a254aa3b3d6cec71925eb3ace76d16
gregoire placca <gregoire.placca@hotmail.com>
2020-02-29 16:16:01 +0100

    last_five

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
     @training = Training.new
-    @last_five_trainings = @team.trainings.last(5).sort_by { |e| e[:start_at]}
+    @last_five_trainings = @team.trainings.order(:start_at).last(5)
     # @last_five_trainings_class = @last_five_trainings.class
 
 
