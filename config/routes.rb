05edfea795c0ec5fb24abea9225681b61e81f02c
gregoire placca <gregoire.placca@hotmail.com>
2020-02-29 15:55:33 +0100

    save session button

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :teams, only: [:show] do
     resources :trainings, only: [:create, :index]
   end
 
-    resources :trainings, only: [:edit, :show] do
-      resources :exercise, only: [:show]
-      resources :training_exercises, only: [:new, :create, :show, :destroy, :update]
+  resources :trainings, only: [:edit, :show] do
+    resources :exercise, only: [:show]
+    resources :training_exercises, only: [:new, :create, :show, :destroy, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'joke' => 'pages#joke', as: :joke
